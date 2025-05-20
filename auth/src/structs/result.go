8db84e31d09bfd8dd947644419d8db90015ac87d
mattuu0 <tempacountyt@gmail.com>
2025-05-20 16:10:27 +0900

    リモートのイメージを使うように変更

--- auth/src/structs/result.go
+++
@@ -1,8 +0,0 @@
-package structs
-
-type HttpResult struct {
-	Code    int      `json:"code"`
-	Message string `json:"message"`
-	Error   error    `json:"error"`
-	Success bool     `json:"success"`
-}
--- auth/src/utils/nowtime.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "time"
-
-func NowTime() int64 {
-	return int64(time.Now().Unix())
-}
