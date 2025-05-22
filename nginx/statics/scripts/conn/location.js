fc0f85aedf892b4cf4cb868e59895d9758b19825
mattuu0 <tempacountyt@gmail.com>
2025-05-22 11:55:07 +0900

    トーストを出すように変更

--- nginx/statics/scripts/conn/location.js
+++ nginx/statics/scripts/conn/location.js
@@ -9,29 +9,56 @@ locationInterval = setInterval(async () => {
     const location = GetLocation();
 
     // post で送信
-    const req = await fetch("/location/update",{
+    const req = await fetch("/location/update", {
         method: "POST",
         headers: {
             "Content-type": "application/json",
             "Authorization": jwtToken
         },
         body: JSON.stringify({
-            "lat" : location.latitude,
-            "lng" : location.longitude,
+            "lat": location.latitude,
+            "lng": location.longitude,
         })
     });
 
     const result = await req.json();
     result["near"].forEach(nUser => {
+        console.log(nUser);
+
         // ピンが存在するか判定
         if (ExistPin(nUser["userid"])) {
             // 存在するとき動かす
-            MovePin(nUser["userid"],nUser["latitude"],nUser["longitude"]);
+            MovePin(nUser["userid"], nUser["latitude"], nUser["longitude"]);
             return;
         }
 
+
+        // トーストのオプション
+        toastr.options = {
+            "closeButton": true,
+            "debug": true,
+            "newestOnTop": true,
+            "progressBar": true,
+            "positionClass": "toast-top-center",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+
+        // トーストを出す
+        toastr["info"](nUser["userid"] + "さんが近くにいます", "通知")
+
+        console.log(nUser["userid"] + "さんが近くにいます");
+
         // 近くにいるフレンド表示
-        NewPin(nUser["userid"],GetIcon(nUser["userid"]),nUser["latitude"],nUser["longitude"]);
+        NewPin(nUser["userid"], GetIcon(nUser["userid"]), nUser["latitude"], nUser["longitude"]);
     });
 
     // 離れたフレンドのピンを消す
