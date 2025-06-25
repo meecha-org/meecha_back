5b167648e7d89eaa632e461053885c97c7d2ff44
mattuu0 <tempacountyt@gmail.com>
2025-06-25 22:43:07 +0900

    いったん現在の構成をすべて削除

--- location/src/utils/nowtime.go
+++
@@ -1,7 +0,0 @@
-package utils
-
-import "time"
-
-func NowTime() int64 {
-	return time.Now().Unix()
-}
--- nginx/statics/scripts/conn/message.js
+++
@@ -1,5 +0,0 @@
-// 認証に使用するメッセージ
-const AuthMessage = "Auth";
-
-// 位置情報メッセージ
-const GeoMessage = "GeoData";
