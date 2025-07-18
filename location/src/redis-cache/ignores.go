c2d5f2780203e6b814469d1e9129e063d9ff8460
mattuu0 <tempacountyt@gmail.com>
2025-07-19 03:30:03 +0900

    存在しない場合 キャッシュから削除を追加

--- location/src/redis-cache/ignores.go
+++ location/src/redis-cache/ignores.go
@@ -120,3 +120,19 @@ func ExistCacheIgnores(args CacheIgnoreArgs) bool {
 
 	return false
 }
+
+// キャッシュから削除する
+func DeleteIgnoreCahce(userID, ignoreID string) error {
+	// バックグラウンドコンテキスト
+	ctx := context.Background()
+
+	// redis に保存
+	_, err := IgnoreRedisConn.ZRem(ctx, userID, ignoreID).Result()
+
+	// エラー処理
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
